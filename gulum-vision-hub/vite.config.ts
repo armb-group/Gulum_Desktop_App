26cad3ac9abf25deab093081c869d52d187d4ffa
CodeAritraDhank <aridhank21205@gmail.com>
2026-06-16 11:59:19 +0530

    vite api fixed

--- gulum-vision-hub/vite.config.ts
+++ gulum-vision-hub/vite.config.ts
@@ -10,11 +10,12 @@ export default defineConfig(({ mode }) => ({
     // port: 8080,
     port: 8082,
     proxy: {
-      "^/(auth|notice|teachers|assignments|syllabus|v1|api|departments|academic-batches|subjects|upload|schedule|attendance|course-class)": {
-        target: "http://localhost:3000",
+      '/gulum': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-      },
+        rewrite: (path) => path.replace(/^\/gulum/, ''),
+      }
     },
     hmr: {
       overlay: false,
